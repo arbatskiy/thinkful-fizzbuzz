0bd8e88b4ad00479752f2a5aafb761b85e76de46
Artem Arbatskiy <artyom.arbatskiy@gmail.com>
2016-10-13 00:39:03 +0700

    Add document ready

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-for (var i = 0; i <= 15; i++) {
-	if (i % 3 === 0 && i % 5 === 0) {
-		$('body').append('<p><strong>FizzBuzz</strong></p>');
-	} else if (i % 3 === 0) {
-		$('body').append('<p><strong>Fizz</strong></p>');
-	} else if (i % 5 === 0) {
-		$('body').append('<p><strong>Buzz</strong></p>');
-	} else {
-		var html = "<p><strong>" + i + "</strong></p>";
-		$('body').append(html);
+$(document).ready(function() {
+	for (var i = 0; i <= 15; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			$('body').append('<p><strong>FizzBuzz</strong></p>');
+		} else if (i % 3 === 0) {
+			$('body').append('<p><strong>Fizz</strong></p>');
+		} else if (i % 5 === 0) {
+			$('body').append('<p><strong>Buzz</strong></p>');
+		} else {
+			var html = "<p><strong>" + i + "</strong></p>";
+			$('body').append(html);
+		}
 	}
-}
+});
